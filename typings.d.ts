8ff2d818bc2fcdb48d88fb4c4a025070e79c97c0
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:45 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -170,10 +170,35 @@ declare module 'react-native-ico-ui-interface' {
       'wall-clock' |
       'padlock';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -181,4 +206,3 @@ declare module 'react-native-ico-ui-interface' {
     export { iconNames };
     export default Icon;
 }
-
