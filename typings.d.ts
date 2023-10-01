980a3b4cc2067e87267e45b21043e2affa739ef9
Dimitry Ivanov <2@ivanoff.org.ua>
2023-10-01 18:37:12 +0300

    fix types

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-ico-ui-interface' {
     import { ReactNode } from 'react';
+    import { SvgProps, AdditionalProps } from 'react-native-svg';
 
     type iconNames = 'monitor' |
       'smile-1' |
@@ -201,7 +202,7 @@ declare module 'react-native-ico-ui-interface' {
       badge?: number | string | badgeTypeObj;
     }
 
-    const Icon: (props: IconProps) => ReactNode;
+    const Icon: (props: IconProps & SvgProps & AdditionalProps) => ReactNode;
 
     export { iconNames };
     export default Icon;
